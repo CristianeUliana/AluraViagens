a7b163b85282461dacd1a171d7c91559d95fbfdc
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-15 22:52:47 -0300

    Módulo 2 - Parte 1 - Layout Iphone SE finalizado

---
+++ AluraViagens/Home/View/TableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TableViewCell.swift
+//  AluraViagens
+//
+//  Created by Cristiane Goncalves Uliana on 15/03/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    @IBOutlet weak var labelQuantidadeDias: UILabel!
+    @IBOutlet weak var labelPreco: UILabel!
+    @IBOutlet weak var imagemViagem: UIImageView!
+
+    
+}
