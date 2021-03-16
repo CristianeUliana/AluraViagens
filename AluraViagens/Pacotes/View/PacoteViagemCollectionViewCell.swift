aee61222b96616eaeada7795ad82b5fad7cc5bdd
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-16 18:27:14 -0300

    Módulo 2 - Parte 2 - Search Bar implementada - aguardando alteração nos constraints da célula da parte 1

---
+++ AluraViagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  AluraViagens
+//
+//  Created by Cristiane Goncalves Uliana on 16/03/21.
+//  Copyright © 2021 Alura. All rights reserved.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    
+    @IBOutlet weak var labelPreco: UILabel!
+    
+}
