463c0968b9e7805432235f2929eb1345d37c54ce
Cristiane Goncalves Uliana <cristianeguliana@gmail.com>
2021-03-17 17:51:54 -0300

    Módulo 2 finalizado, aplicativo implementado para iphone e ipad

--- AluraViagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
+++ AluraViagens/DetalhesViagem/Controller/DetalhesViagensViewController.swift
@@ -44,7 +44,9 @@ class DetalhesViagensViewController: UIViewController {
     
     
     @IBAction func botaoVoltar(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
+        if let navigation = self.navigationController {
+            navigation.popToRootViewController(animated: true)
+         }
     }
     
     
@@ -61,5 +63,11 @@ class DetalhesViagensViewController: UIViewController {
         self.textFieldData.text = formatador.string(from: sender.date)
     }
     
-  
+    @IBAction func botaoFinalizarCompra(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "confirmacaoPagamento") as! ConfirmacaoPagamentoViewController
+        controller.pacoteComprado = pacoteSelecionado
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
 }
--- AluraViagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ AluraViagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -27,8 +27,6 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
         self.labelContadorPacotes.text = self.atualizaContadorLabel()
     }
     
-    
-    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.listaViagens.count
     }
@@ -36,31 +34,21 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
         let pacoteAtual = listaViagens[indexPath.item]
-        celulaPacote.labelTitulo.text = pacoteAtual.viagem.titulo
-        celulaPacote.labelQuantidadeDeDias.text = "\(pacoteAtual.viagem.quantidadeDeDias) dias"
-        celulaPacote.labelPreco.text = "R$ \(pacoteAtual.viagem.preco)"
-        celulaPacote.imagemViagem.image = UIImage(named: pacoteAtual.viagem.caminhoDaImagem)
-        celulaPacote.layer.borderWidth = 0.5
-        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
-        celulaPacote.layer.cornerRadius = 8
+        celulaPacote.configuraCelula(pacoteViagem: pacoteAtual)
         return celulaPacote
     }
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let larguraCelula = collectionView.bounds.width / 2
-        return CGSize(width: larguraCelula - 15, height: 160)
+        return UIDevice.current.userInterfaceIdiom == .phone ? CGSize(width: collectionView.bounds.width/2-20, height: 160) : CGSize(width: collectionView.bounds.width/3-20, height: 250)
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
         let pacote = listaViagens[indexPath.item]
-        
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "detalhes") as! DetalhesViagensViewController
         controller.pacoteSelecionado = pacote
-        self.present(controller, animated: true, completion: nil)
+        self.navigationController?.pushViewController(controller, animated: true)
     }
-    
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
@@ -78,6 +66,5 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     func atualizaContadorLabel() -> String {
         return listaViagens.count == 1 ? "1 pacote encontrado" : "\(listaViagens.count) pacotes encontrados"
     }
-    
     
 }
